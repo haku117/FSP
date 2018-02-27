7c43cb7cb486b0243eaa55a59a1bb372395ed305
haku117 <haku7117@gmail.com>
2018-02-27 18:21:25 -0500

    first commit

---
+++ src/examples/hybridgraph/examples/kmeans/distributed/KmeansValue.java
@@ -0,0 +1,48 @@
+package hybridgraph.examples.kmeans.distributed;
+
+public class KmeansValue {
+	private int tag;
+	private double distance;
+	
+	public KmeansValue() {
+		this.tag = -1;
+		this.distance = -1.0;
+	}
+	
+	public void set(int _tag, double _distance) {
+		this.tag = _tag;
+		this.distance = _distance;
+	}
+	
+	public int getTag() {
+		return this.tag;
+	}
+	
+	public double getDistance() {
+		return this.distance;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append(tag);
+		sb.append(":");
+		sb.append(distance);
+		return sb.toString();
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (!(o instanceof KmeansValue)) {
+			return false;
+		}
+		
+		KmeansValue other = (KmeansValue) o;
+		if (this.tag==other.tag 
+				&& this.distance==other.distance) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
